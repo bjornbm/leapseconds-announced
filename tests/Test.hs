9b2e0c5333455dc57ffc58cd8074ae245db46ec1
Björn Buckwalter <bjorn@buckwalter.se>
2017-07-30 21:35:22 +0200

    Update test suite for LeapsecondMap and recent years.

--- tests/Test.hs
+++ tests/Test.hs
@@ -8,11 +8,14 @@ onceCheck = quickCheckWith (stdArgs {maxSuccess = 1})
 -- A few trivial tests.
 main :: IO ()
 main = do
-  onceCheck $ lst (fromGregorian 1111 12 31) ==  0  -- Before first leap second.
-  onceCheck $ lst (fromGregorian 2008 12 31) == 33
-  onceCheck $ lst (fromGregorian 2009 01 01) == 34
-  onceCheck $ lst (fromGregorian 2009 12 31) == 34
-  onceCheck $ lst (fromGregorian 2012 06 30) == 34  -- Prior to last leap second.
-  onceCheck $ lst (fromGregorian 2012 07 01) == 35  -- Last leap second.
-  onceCheck $ lst (fromGregorian 2012 12 31) == 35  -- Beyond last leap second.
+  onceCheck $ lst (fromGregorian 1111 12 31) == Just  0  -- Before first leap second.
+  onceCheck $ lst (fromGregorian 2008 12 31) == Just 33
+  onceCheck $ lst (fromGregorian 2009 01 01) == Just 34
+  onceCheck $ lst (fromGregorian 2009 12 31) == Just 34
+  onceCheck $ lst (fromGregorian 2012 06 30) == Just 34
+  onceCheck $ lst (fromGregorian 2012 07 01) == Just 35
+  onceCheck $ lst (fromGregorian 2012 12 31) == Just 35
+  onceCheck $ lst (fromGregorian 2016 12 31) == Just 36  -- Prior to last leap second.
+  onceCheck $ lst (fromGregorian 2017 01 01) == Just 37  -- Last leap second.
+  onceCheck $ lst (fromGregorian 2017 12 31) == Just 37  -- Beyond last leap second.
 
